9ab998a0b100c0bb366cf2c41a350529d4e81183
LDLNAN <lucas-donald@live.nmit.ac.nz>
2025-02-24 11:21:50 +1300

    Playing with Index.js, numberssss

--- Index.js
+++ Index.js
@@ -5,3 +5,25 @@ console.log("Hello")
  */
 
 // seeing how "Blame" works
+const cNumber = 3.5
+let number1 = 2
+let number2 = 0
+
+console.log("Init Numbers: ", cNumber, number1, number2)
+number2 = multiply(number1, cNumber)
+console.log("Final Result:", number2)
+
+number2 = divide(number2, number1)
+console.log("Result: ", number2)
+
+
+function multiply(num1, num2)
+{
+    console.log("Multiplying ", num1, "by ", num2)
+    return num1 * num2
+}
+function divide(num1, num2)
+{
+    console.log("Dividing ", num1, "by ", num2)
+    return num1 / num2
+}
